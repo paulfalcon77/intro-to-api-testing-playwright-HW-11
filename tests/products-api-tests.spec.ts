4d11c6707cdf47d011804975c17dbc92009c4ce1
Pavel Sokolov <pavel.sok77@gmail.com>
2026-03-26 10:15:24 +0200

    HW_11_DTO

--- tests/products-api-tests.spec.ts
+++ tests/products-api-tests.spec.ts
@@ -1,23 +1,20 @@
 import { expect, test } from '@playwright/test'
-
+import { Product } from './models/Product.dto'
 import { StatusCodes } from 'http-status-codes'
+
 
 test.describe('Lesson 11 -> Product API tests', () => {
   const BaseEndpointURL = 'https://backend.tallinn-learning.ee/products'
   const AUTH = { 'X-API-Key': 'my-secret-api-key' }
-  type Product = {
-    id: number
-    name: string
-    price: number
-    createdAt: string | null
-  }
+
 
   test('GET /products - check API returns array with length >= 1', async ({ request }) => {
     const response = await request.get(BaseEndpointURL, {
       headers: AUTH,
     })
 
-    const responseBody: Product[] = await response.json()
+    const responseBody = Product.createNewArray(await response.json())
+
     expect(response.status()).toBe(StatusCodes.OK)
     expect(responseBody.length).toBeDefined()
     expect(responseBody.length).toBeGreaterThanOrEqual(1)
@@ -26,28 +23,29 @@ test.describe('Lesson 11 -> Product API tests', () => {
   test('POST /products; GET /products/{id} - check product creation and product search by id', async ({
     request,
   }) => {
-    const testProduct: Product = {
+    const testProduct = new Product ({
       id: 0,
       name: 'test lesson 11',
       price: 124523643,
       createdAt: '2026-03-23T18:04:11.285Z',
-    }
+    })
 
     const createResponse = await request.post(BaseEndpointURL, {
       headers: AUTH,
       data: testProduct,
     })
 
-    const createResponseBody: Product = await createResponse.json()
+    const createResponseBody = new Product(await createResponse.json())
     expect(createResponseBody.id).toBeGreaterThan(0)
     expect(createResponseBody.name).toBe(testProduct.name)
     expect(createResponseBody.price).toBe(testProduct.price)
     expect(createResponseBody.createdAt).toBeDefined()
+    expect(createResponseBody).toBeInstanceOf(Product)
 
     const searchResponse = await request.get(`${BaseEndpointURL}/${createResponseBody.id}`, {
       headers: AUTH,
     })
-    const searchResponseBody: Product = await searchResponse.json()
+    const searchResponseBody = new Product( await searchResponse.json())
     expect(searchResponse.status()).toBe(StatusCodes.OK)
     expect.soft(searchResponseBody.id).toBe(createResponseBody.id)
     expect.soft(searchResponseBody.name).toBe(testProduct.name)
@@ -75,7 +73,7 @@ test.describe('Lesson 11 -> Product API tests', () => {
       headers: AUTH,
       data: testProduct,
     })
-    const createResponseBody: Product = await createResponse.json()
+    const createResponseBody = new Product(await createResponse.json())
 
     const deleteResponse = await request.delete(`${BaseEndpointURL}/${createResponseBody.id}`, {
       headers: AUTH,
@@ -101,7 +99,7 @@ test.describe('Lesson 11 -> Product API tests', () => {
     })
     expect(createProductResponse.status()).toBe(StatusCodes.OK)
 
-    const createProductResponseBody: Product = await createProductResponse.json()
+    const createProductResponseBody = new Product (await createProductResponse.json())
     console.log(createProductResponseBody)
 
     const newId = createProductResponseBody.id
@@ -119,7 +117,7 @@ test.describe('Lesson 11 -> Product API tests', () => {
     })
     expect(updateResponse.status()).toBe(StatusCodes.OK)
 
-    const updateResponseBody: Product = await updateResponse.json()
+    const updateResponseBody = new Product(await updateResponse.json())
     console.log(updateResponseBody)
 
     expect.soft(updateResponseBody.name).toBe(updateProduct.name)
@@ -129,7 +127,7 @@ test.describe('Lesson 11 -> Product API tests', () => {
     const searchResponse = await request.get(`${BaseEndpointURL}/${updateResponseBody.id}`, {
       headers: AUTH,
     })
-    const searchResponseBody: Product = await searchResponse.json()
+    const searchResponseBody = new Product(await searchResponse.json())
     expect.soft(searchResponse.status()).toBe(StatusCodes.OK)
     expect.soft(searchResponseBody.id).toBe(updateResponseBody.id)
     expect.soft(searchResponseBody.name).toBe(updateProduct.name)
