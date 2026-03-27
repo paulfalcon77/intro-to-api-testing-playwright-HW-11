ed326c4c0ef08799053169ffde1ae7c6cfc5b1a4
Pavel Sokolov <pavel.sok77@gmail.com>
2026-03-27 12:50:10 +0200

    format with prettier

--- tests/login_api_tests.spec.ts
+++ tests/login_api_tests.spec.ts
@@ -2,13 +2,13 @@ import { expect, test } from '@playwright/test'
 
 test.describe('Login API tests', () => {
   const BaseEndPoint = 'https://backend.tallinn-learning.ee/login/student'
-  test('Incorrest login ', async ({request}) => {
+  test('Incorrest login ', async ({ request }) => {
     const LoginResponse = await request.post(BaseEndPoint, {
       data: {
         username: '',
         password: '',
-      }
-    });
-    expect(LoginResponse.status()).toBe(401);
+      },
+    })
+    expect(LoginResponse.status()).toBe(401)
   })
 })
